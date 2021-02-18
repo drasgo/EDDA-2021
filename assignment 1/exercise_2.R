201f21460f2bc9ab94e7e2068b9e8f29d0883c6b
drasgo <tommasocastiglione@gmail.com>
2021-02-19 00:03:41 +0100

    Added ex2 and ex4

--- assignment 1/exercise_2.R
+++ assignment 1/exercise_2.R
@@ -3,3 +3,58 @@
 # Created by: drasgo
 # Created on: 16/02/21
 
+p.value=function(n,m,mu,nu,sd,B=1000){
+  p=numeric(B) # p will be an array of realized p-values
+  for (b in 1:B) {x=rnorm(n,mu,sd); y=rnorm(m,nu,sd)
+  p[b]=t.test(x,y,var.equal=TRUE)[[3]]}
+  return(p)}
+
+
+get_power=function(n,m,mu,sd,B, sequence){
+  power = numeric(length(sequence))
+  
+  for (index in 1:length(sequence)){
+    nu_val <- sequence[index]
+    p <- numeric(B)
+    
+    for (b in 1:B) {
+      x<-rnorm(n,mu,sd);
+      y<-rnorm(m,nu_val,sd)
+      p[b] <- t.test(x,y,var.equal=TRUE)[[3]]
+    }
+    
+    power[index] <- mean(p.value(n, m, mu, nu_val, sd, B)<0.05)
+  }
+  return(power)
+}
+
+par(mfrow=c(1,1))
+
+"A"
+n <- 30
+m <- 30
+mu <- 180
+sd <- 5
+B <- 1000
+seq <-seq(175, 185, by=0.25)
+
+pow_a = get_power(n,m,mu,sd,B, seq)
+plot(pow_a, seq)
+"hist(power)
+qqnorm(power)"
+
+"B"
+n <- 100
+m <- 100
+
+pow_b = get_power(n,m,mu,sd,B,seq)
+plot(pow_b, seq)
+
+"C"
+n<-30
+m<-30
+sd<-15
+
+pow_c = get_power(n,m,mu,sd,B,seq)
+plot(pow_c, seq)
+
--- assignment 1/exercise_4.R
+++ assignment 1/exercise_4.R
@@ -3,3 +3,44 @@
 # Created by: drasgo
 # Created on: 16/02/21
 
+data=read.table(file="run.txt",header=TRUE)
+print(data)
+
+"A: paired t test"
+print(data[,1])
+t.test(data[,1],data[,2],paired=TRUE)
+qqnorm(data[,1]-data[,2])
+
+"B: pearson correlation"
+lemo <- data[1:12, 1:2]
+energy <- data[13:24, 1:2]
+
+qqnorm(lemo[,1])
+qqnorm(lemo[,2])
+cor.test(lemo[, 1], lemo[, 2])
+
+qqnorm(energy[,1])
+qqnorm(energy[,2])
+cor.test(energy[,1], energy[,2])
+
+"C: hypothesis is 'there is no difference', so p>0.05 ==> there is a difference"
+
+lemo_diff = lemo[,1] - lemo[,2]
+energy_diff = energy[,1] - energy[,2]
+
+mystat=function(x,y) {mean(x-y)}
+B=1000
+tstar=numeric(B)
+for (i in 1:B)
+{
+  datastar=t(apply(cbind(lemo_diff,energy_diff),1,sample))
+  tstar[i]=mystat(datastar[,1],datastar[,2])
+}
+myt=mystat(lemo_diff,energy_diff)
+
+myt
+hist(tstar)
+pl=sum(tstar<myt)/B
+pr=sum(tstar>myt)/B
+p=2*min(pl,pr)
+print(p)
