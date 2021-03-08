30a85f62804caf8a86b1238a5602610adc17d995
drasgo <tommasocastiglione@gmail.com>
2021-03-08 10:04:18 +0100

    Added folder for assignemnt 2

--- assignment 1/exercise_2.R
+++ assignment 1/exercise_2.R
@@ -2,7 +2,6 @@
 # Objective : TODO
 # Created by: drasgo
 # Created on: 16/02/21
-
 p.value=function(n,m,mu,nu,sd,B=1000){
   p=numeric(B) # p will be an array of realized p-values
   for (b in 1:B) {x=rnorm(n,mu,sd); y=rnorm(m,nu,sd)
@@ -28,7 +27,7 @@ get_power=function(n,m,mu,sd,B, sequence){
   return(power)
 }
 
-par(mfrow=c(1,1))
+par(mfrow=c(1, 2))
 
 "A"
 n <- 30
@@ -36,10 +35,10 @@ m <- 30
 mu <- 180
 sd <- 5
 B <- 1000
-seq <-seq(175, 185, by=0.25)
+nu <-seq(175, 185, by=0.25)
 
-pow_a = get_power(n,m,mu,sd,B, seq)
-plot(pow_a, seq)
+power = get_power(n,m,mu,sd,B, nu)
+plot(power, nu, main="Graph 2.1")
 "hist(power)
 qqnorm(power)"
 
@@ -47,14 +46,15 @@ qqnorm(power)"
 n <- 100
 m <- 100
 
-pow_b = get_power(n,m,mu,sd,B,seq)
-plot(pow_b, seq)
+power = get_power(n,m,mu,sd,B,nu)
+plot(power, nu, main = "Graph 2.2")
 
 "C"
+par(mfrow=c(1, 1))
 n<-30
 m<-30
 sd<-15
 
-pow_c = get_power(n,m,mu,sd,B,seq)
-plot(pow_c, seq)
+power = get_power(n,m,mu,sd,B,nu)
+plot(power, nu, main="Graph 2.3")
 
