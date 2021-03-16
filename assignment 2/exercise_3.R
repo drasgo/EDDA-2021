732e94e032778286497a945fe329e09c94ab8b34
drasgo <tommasocastiglione@gmail.com>
2021-03-16 11:02:17 +0100

    Added ass 3 and files

--- assignment 2/exercise_3.R
+++ assignment 2/exercise_3.R
@@ -3,7 +3,7 @@ library(lme4)
 
 cow=read.table(file="cow.txt",header=TRUE)
 attach(cow)
-
+cow
 
 #Point 1: fixed effects
 cow$id=factor(cow$id);
@@ -11,13 +11,16 @@ cow$milk=factor(cow$milk)
 cow$id <- as.numeric(as.character(cow$id)) 
 cow$milk <- as.numeric(as.character(cow$milk))
 
-cowlm=lm(milk~treatment+per+id,data=cow)
+cowlm=lm(milk~per+id+treatment,data=cow)
 anova(cowlm)
 summary(cowlm)
 
 #Point 2: mixed effects
-cowlmer=lmer(milk~treatment+order+per+(1|id),REML=FALSE)
+cowlmer=lmer(milk~treatment+order+per+(1|id),data=cow,REML=FALSE)
 summary(cowlmer)
+
+cowlmer1=lmer(milk~order+per+(1|id),data=cow,REML=FALSE)
+anova(cowlmer1,cowlmer)
 
 
 #Point 3: t-test paired
