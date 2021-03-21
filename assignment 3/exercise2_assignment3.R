600884db144a15794b632067beae3b8ef42b835a
drasgo <tommasocastiglione@gmail.com>
2021-03-21 19:05:01 +0100

    :..

--- assignment 3/exercise2_assignment3.R
+++ assignment 3/exercise2_assignment3.R
@@ -1,8 +1,12 @@
 titanic=read.table(file="titanic.txt",header=TRUE)
 attach(titanic)
 titanic
+par(mfrow=c(1, 2))
 
-# a (Fatto)
+
+
+# **** a. Fatto
+
 titanic$Age = as.numeric(titanic$Age)
 titanic$PClass = as.numeric(titanic$PClass)
 hist(titanic[,3],main="Age")
@@ -24,14 +28,16 @@ round(tot/xtabs(~titanic$PClass+titanic$Sex), 2)
 summary(titanic)
 
 
-#b (Fatto)
-# titanic
+
+
+# **** b. (Fatto)
+
 # titanic$Survived
 # titanic$Survived = factor(titanic$Survived)
 # titanic$PClass = factor(titanic$PClass)
 # titanic$Age = factor(titanic$Age)
-titanic$Sex = as.numeric(titanic$Sex)
-titanic$PClass = as.numeric(titanic$PClass)
+titanic$Sex = factor(titanic$Sex)
+titanic$PClass = factor(titanic$PClass)
 titanic$Age = as.numeric(titanic$Age)
 call <- glm(Survived~PClass+Age+Sex,family=binomial, data=titanic)
 #call1 <- glm(Survived~PClass+Sex,family=binomial, data=titanic)
@@ -41,45 +47,46 @@ summary(call)
 # the estimated odds are calculated as reported by summary(call)
 
 
-# c Mi manca "scegliere" modello e testarlo  su factors di PClass e Sex con Age == 53
+# **** c. Fatto
+
 call1_alternativa <- glm(Survived~Age*(PClass+Sex),family=binomial, data=titanic)
-call1 <- glm(Survived~Age*PClass*Sex,family=binomial, data=titanic)
+anova(call1_alternativa, test="Chisq")
 
-anova(call1, test="Chisq")
-summary(call1)
+# No interaction between age pc class and sex, but interaction between age and sex
 
 
-# Capire perchè questo modello
-# titanic$PClass = factor(titanic$PClass)
-# titanic$Sex = factor(titanic$Sex)
+
+# guardare interazioni tra factors
 titanic$Survived = factor(titanic$Survived)
 titanic$PClass = factor(titanic$PClass)
 titanic$Sex = factor(titanic$Sex)
 titanic$Age = as.numeric(titanic$Age)
-sur_glm_4= glm(Survived~PClass+Age*Sex,data=data,family=binomial)
-
-newdata = data.frame(Age=53)
-predict(model, newdata, type="response")
-#probabilità
-P(survival=1) = odds / (1+odds)
+model = glm(Survived~PClass+Age*Sex,data=titanic,family=binomial)
+summary(model)
+fitted(model)
 
 
-# No interaction between age pc class and sex, but interaction between pc class and sex
+for (pc in levels(titanic$PClass)){
+  for (sex in levels(titanic$Sex)) {
+    newdata = data.frame(Sex=sex, PClass=pc, Name="Mr. Mc Buttface", Age=53)
+    print(newdata)
+    print(predict(model, newdata, type="response"))
+    }
+}
 
-# titanic = titanic[titanic$Age==53.00,]
-call2 <- glm(Survived~PClass+Age+Sex,family=binomial, data=titanic)
-drop1(call2,test="Chisq")
-summary(call2)
-shortglm=glm(cbind(ncases,ncontrols)~age+age2+alc+tob,data=titanic,family=binomial)
 
-# d Ripetere parte di Machine Learning
 
-# e Fatto
+# **** d. Ripetere parte di Machine Learning 
+# Much higher than p_0 or much lower than p_0, P gives a prediction with more confidence
+
+
+
+# **** e. Fatto
 
 #Chisquare for pclass + survived and fisher for sex + survived
 
 chisq.test(titanic$PClass, titanic$Survived)
-fisher.test(titanic$Sex, titanic$Survived)
+chisq.test(titanic$Sex, titanic$Survived)
 
 
 #call <- glm(Survived~PClass+Age+Sex,family=binomial, data=titanic)
@@ -91,7 +98,9 @@ fisher.test(titanic$Sex, titanic$Survived)
 #anova(call2, call)
 #anova(call3, call)
 
-#oppure
 
-# f Da fare
+# **** f. Da fare
+
+# chisquare and fisher only give an idea on the independence of two variables but nothing more on the actual influence + can be inaccurate for too little samples
+
 
